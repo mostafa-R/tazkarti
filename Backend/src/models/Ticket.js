c68b842605d432007cdf6705f906010f631a5f8f
zyad <zyad.890@gmail.com>
2025-07-05 22:56:02 +0300

    Add Ticket model

---
+++ Backend/src/models/Ticket.js
@@ -0,0 +1,32 @@
+
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+    },
+    status: {
+      type: String,
+      enum: ["open", "in progress", "closed"],
+      default: "open",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Ticket = mongoose.model("Ticket", ticketSchema);
