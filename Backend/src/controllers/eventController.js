e6f9d82812d3443519b09b839e8bd05304f77bc3
Zyad Saeed <zeyaad.890@gmail.com>
2025-07-21 20:32:09 +0300

    Update eventController

---
+++ Backend/src/controllers/eventController.js
@@ -0,0 +1,22 @@
+import { Event } from "../models/Event.js";
+
+export const createEvent = async (req, res) => {
+  try {
+    const event = await Event.create({ ...req.body, organizer: req.user._id });
+    res.status(201).json(event);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getAllEvents = async (req, res) => {
+  try {
+const events = await Event.find().populate("organizer", "name email");
+
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
